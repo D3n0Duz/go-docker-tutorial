96623709ccdaf5d8a7884d67a66a842edb280e50
D3n0Duz <denoduz@gmail.com>
2019-04-29 23:33:40 -0400

    Test with firebase db

--- alpine/controllers/AccountController.go
+++ alpine/controllers/AccountController.go
@@ -19,6 +19,35 @@ func (controller *AccountController) GetAccount(w http.ResponseWriter, r *http.R
 
 	fmt.Fprint(w, "GET "+accountID)
 
+	/*
+		sa := option.WithCredentialsFile("./serviceAccountKey.json")
+		ctx := context.Background()
+		app, err := firebase.NewApp(ctx, nil, sa)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		client, err := app.Firestore(ctx)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer client.Close()
+
+		iter := client.Collection("accounts").Documents(ctx)
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Failed to iterate: %v", err)
+			}
+			fmt.Println(doc.Data())
+		}
+	*/
+
+	///////////////////////////////////
+
 	/*scores, err := controller.PlayerService.GetScores(player1Name, player2Name)
 	if err != nil {
 		//Handle error
