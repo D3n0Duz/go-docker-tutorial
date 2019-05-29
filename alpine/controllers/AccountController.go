1d16637fe1f1ea273da466a9a16fa042fe4a51e6
edouard vilver <edouard.vilver@gmail.com>
2019-05-28 23:26:47 -0400

    Add token validation

--- alpine/controllers/AccountController.go
+++ alpine/controllers/AccountController.go
@@ -2,19 +2,27 @@ package controllers
 
 import (
 	"fmt"
+	"strings"
 	"net/http"
 	"../interfaces"
 	"encoding/json"
 	"../models"
 	"github.com/go-chi/chi"
+	"github.com/dgrijalva/jwt-go"
 )
 
 type AccountController struct {
 	AccountService interfaces.IAccountService
-	
+
 }
 
+const jwtKey = "YOUR_JWT_KEY" // TODO : get key from Vault
+
 func (controller *AccountController) GetAccount(w http.ResponseWriter, r *http.Request) {
+
+	if !VerifyToken(r, w) {
+		return // No valid token found
+	}
 
 	accountID := chi.URLParam(r, "accountid")
 
@@ -32,6 +40,10 @@ func (controller *AccountController) GetAccount(w http.ResponseWriter, r *http.R
 
 func (controller *AccountController) PostAccount(w http.ResponseWriter, r *http.Request) {
 
+	if !VerifyToken(r, w) {
+		return // No valid token found
+	}
+
 	var accountModel models.AccountModel
 	json.NewDecoder(r.Body).Decode(&accountModel)
 
@@ -48,6 +60,9 @@ func (controller *AccountController) PostAccount(w http.ResponseWriter, r *http.
 }
 
 func (controller *AccountController) PutAccount(w http.ResponseWriter, r *http.Request) {
+	if !VerifyToken(r, w) {
+		return // No valid token found
+	}
 
 	var accountModel models.AccountModel
 	json.NewDecoder(r.Body).Decode(&accountModel)
@@ -66,7 +81,9 @@ func (controller *AccountController) PutAccount(w http.ResponseWriter, r *http.R
 }
 
 func (controller *AccountController) DeleteAccount(w http.ResponseWriter, r *http.Request) {
-
+	if !VerifyToken(r, w) {
+		return // No valid token found
+	}
 	
 	accountID := chi.URLParam(r, "accountid")
 	
@@ -78,4 +95,28 @@ func (controller *AccountController) DeleteAccount(w http.ResponseWriter, r *htt
 	}
 
 	fmt.Fprint(w, "Deleted account : "+accountID)
+}
+
+// TODO create abstract class that can do that validation then go to the rest API calls
+func VerifyToken(r *http.Request, w http.ResponseWriter) bool {
+
+	reqToken := r.Header.Get("Authorization")
+	splitToken := strings.Split(reqToken, "Bearer")
+	if len(splitToken) != 2 {
+		http.Error(w, "Permission denied", 401)
+    	return false
+	}
+
+	reqToken = strings.TrimSpace(splitToken[1])
+
+    token, err := jwt.Parse(reqToken, func(t *jwt.Token) (interface{}, error) {
+        return []byte(jwtKey), nil
+    })
+    if err == nil && token.Valid {
+        fmt.Println("valid token found")
+        return true
+    } else {
+        http.Error(w, "Permission denied", 401)
+        return false
+    }
 }
